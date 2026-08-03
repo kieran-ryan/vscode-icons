f46d2f590f4261e2033c23a7e68fbb6ed6fb955d
kieran-ryan <kierankilkenny@gmail.com>
2026-08-03 18:47:46 +0100

    feat(icon): zensical

--- src/iconsManifest/supportedExtensions.ts
+++ src/iconsManifest/supportedExtensions.ts
@@ -7140,6 +7140,12 @@ export const extensions: IFileCollection = {
       format: FileFormat.svg,
     },
     {
+      icon: 'zensical',
+      extensions: ['zensical.toml'],
+      filename: true,
+      format: FileFormat.svg,
+    },
+    {
       icon: 'zig',
       extensions: [],
       languages: [languages.zig],
